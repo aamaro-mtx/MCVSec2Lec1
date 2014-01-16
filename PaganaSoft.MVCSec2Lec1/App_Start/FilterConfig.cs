f0fe3367bd92f283ee91a6112d1063c587dce35f
Abel Amaro Julian <amarotux@gmail.com>
2014-01-15 20:16:35 -0600

    IFirst Commit.

---
+++ PaganaSoft.MVCSec2Lec1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PaganaSoft.MVCSec2Lec1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PaganaSoft.MVCSec2Lec1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaganaSoft.MVCSec2Lec1.MvcApplication" Language="C#" %>
